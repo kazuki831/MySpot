442c6b25ced7663353eb3a27da922c55b7223767
kazuki <kn0831@gmial.com>
2016-02-24 00:44:05 +0900

    mapboxでの表示を実装

--- MySpot/MySpot/ViewController.swift
+++ MySpot/MySpot/ViewController.swift
@@ -8,16 +8,42 @@
 
 import UIKit
 import Mapbox
-import Eureka
 
 
-class ViewController: FormViewController {
+class ViewController: UIViewController, MGLMapViewDelegate {
 
+    var mapView: MGLMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // initialize the map view
+        mapView = MGLMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
+        
+        // set the map's center coordinate
+        mapView.setCenterCoordinate(CLLocationCoordinate2D(latitude: 38.894368,
+            longitude: -77.036487),
+            zoomLevel: 15, animated: false)
+        view.addSubview(mapView)
+        
+        //ピンの表示
+        let point = MGLPointAnnotation()
+        point.coordinate = CLLocationCoordinate2D(latitude: 38.894368, longitude: -77.036487)
+        point.title = "こんにちは"
+        point.subtitle = "mapboxでの表示です"
+        
+        // Add annotation `point` to the map
+        mapView.addAnnotation(point)
+        
+        mapView.delegate = self
     }
-
+    
+    func mapView(mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
